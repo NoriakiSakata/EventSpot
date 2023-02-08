1cb4427f6510d6e00cdc5869383e2bc39691a9e7
坂田徳明 <led06140@gmail.com>
2023-02-08 22:53:09 +0900

    [#1]ボトムナビゲーションのデザイン修正

--- app/lib/pages/bottom_navigation_page.dart
+++ app/lib/pages/bottom_navigation_page.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/pages/add_event/add_event_page.dart';
 import 'package:helloworld/pages/map/map_page.dart';
 import 'package:helloworld/pages/my_list/my_list_page.dart';
+import 'package:helloworld/themes/app_colors.dart';
 
 class BottomNavigationPage extends StatefulWidget {
-  const  BottomNavigationPage({Key? key}) : super(key: key);
+  const BottomNavigationPage({Key? key}) : super(key: key);
 
   @override
-  State< BottomNavigationPage> createState() => _BottomNavigationPage();
+  State<BottomNavigationPage> createState() => _BottomNavigationPage();
 }
 
-class _BottomNavigationPage extends State< BottomNavigationPage> {
+class _BottomNavigationPage extends State<BottomNavigationPage> {
   static const _screens = [
     MapPage(),
     MyListPage(),
@@ -30,15 +31,40 @@ class _BottomNavigationPage extends State< BottomNavigationPage> {
     return Scaffold(
         body: _screens[_selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: AppColors.text1,
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'マップ'),
-            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'マイリスト'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notifications), label: '追加'),
+          items: <BottomNavigationBarItem>[
+            barItem(iconData: Icons.pin_drop_outlined, index: 0, label: 'マップ'),
+            barItem(iconData: Icons.list_alt, index: 0, label: 'マイリスト'),
+            barItem(iconData: Icons.add, index: 0, label: '追加'),
           ],
           type: BottomNavigationBarType.fixed,
         ));
+  }
+
+  BottomNavigationBarItem barItem({
+    required IconData iconData,
+    required int index,
+    required String label,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        iconData,
+        color: AppColors.text1,
+      ),
+      label: label,
+      activeIcon: Container(
+          width: 65,
+          height: 30,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: AppColors.pink,
+          ),
+          child: Icon(
+            iconData,
+            color: AppColors.text1,
+          )),
+    );
   }
 }
