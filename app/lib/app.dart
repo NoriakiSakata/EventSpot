df695c2c87094d8588bb0006a041e5eee42cf3db
坂田徳明 <led06140@gmail.com>
2023-02-08 22:51:53 +0900

    [#1]material3適応＆プライマリー定義＆カスタムカラー定義

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/pages/bottom_navigation_page.dart';
+import 'package:helloworld/themes/app_colors.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          useMaterial3: true,
+          primarySwatch: AppColors.primaryColor,
         ),
         home: const BottomNavigationPage());
   }
