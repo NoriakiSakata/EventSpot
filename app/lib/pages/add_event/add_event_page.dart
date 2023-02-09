82773d20e2788174b959c3d995d566a7267c2536
坂田徳明 <led06140@gmail.com>
2023-02-09 21:09:05 +0900

    [#3]追加イベント一覧画面

--- app/lib/pages/add_event/add_event_page.dart
+++ app/lib/pages/add_event/add_event_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/gen/assets.gen.dart';
+import 'package:helloworld/themes/app_colors.dart';
 
 class AddEventPage extends StatelessWidget {
   const AddEventPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return Scaffold(
+        floatingActionButton: FloatingActionButton.extended(
+            backgroundColor: AppColors.primaryColor.shade600,
+            icon: const Icon(Icons.add),
+            label: const Text('新規イベントを追加'),
+            onPressed: () {}),
+        appBar: AppBar(title: const Text('追加したイベント')),
+        body: ListView.builder(itemBuilder: (context, index) {
+          return ListTile(
+              leading: Container(
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(Assets.images.sampleEvent.path))),
+              ),
+              title: const Text('バンクシーって誰？展'),
+              subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('開始日 2023/1/10（金）'),
+                    Text('終了日 2023/1/11（土）'),
+                  ]),
+              trailing: ElevatedButton(
+                child: const Text('編集'),
+                onPressed: () {},
+              ));
+        }));
   }
 }
